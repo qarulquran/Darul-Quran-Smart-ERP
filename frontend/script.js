8fb397a8af477edca65ffcc497812bb557b569c5
qarulquran <darulquranam@gmail.com>
2026-08-17 18:47:52 +0600

    Add login authentication function

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,21 @@
+function login(){
 
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+
+    if(username === "admin" && password === "12345"){
+
+        alert("Login Successful");
+
+        window.location.href = "dashboard.html";
+
+    }
+
+    else{
+
+        alert("Invalid Username or Password");
+
+    }
+
+}
