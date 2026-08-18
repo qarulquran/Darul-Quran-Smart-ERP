16dc69eaa0d64f7a27fa5677dfa88ac8cd6b0089
qarulquran <darulquranam@gmail.com>
2026-08-18 08:00:00 +0600

    Add dashboard chart functionality

--- frontend/script.js
+++ frontend/script.js
@@ -18,4 +18,118 @@ function login(){
 
     }
 
+}// Student Statistics Chart
+
+const studentChart = document.getElementById("studentChart");
+
+if(studentChart){
+
+new Chart(studentChart, {
+
+type: "bar",
+
+data: {
+
+labels: [
+"Class 1",
+"Class 2",
+"Class 3",
+"Class 4",
+"Class 5"
+],
+
+datasets:[{
+
+label:"Students",
+
+data:[
+80,
+120,
+100,
+150,
+50
+]
+
+}]
+
+}
+
+});
+
+}
+
+
+
+// Attendance Chart
+
+const attendanceChart = document.getElementById("attendanceChart");
+
+if(attendanceChart){
+
+new Chart(attendanceChart, {
+
+type:"pie",
+
+data:{
+
+labels:[
+"Present",
+"Absent"
+],
+
+datasets:[{
+
+data:[
+90,
+10
+]
+
+}]
+
+}
+
+});
+
+}
+
+
+
+// Finance Chart
+
+const financeChart = document.getElementById("financeChart");
+
+if(financeChart){
+
+new Chart(financeChart, {
+
+type:"line",
+
+data:{
+
+labels:[
+"Jan",
+"Feb",
+"Mar",
+"Apr",
+"May"
+],
+
+datasets:[{
+
+label:"Income",
+
+data:[
+30000,
+45000,
+50000,
+60000,
+70000
+]
+
+}]
+
+}
+
+});
+
 }
