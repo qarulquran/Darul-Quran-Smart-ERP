3cce6d1454d0e9397fc92f2ddd3778b5e9839ea0
qarulquran <darulquranam@gmail.com>
2026-08-18 11:37:50 +0600

    Add student save function

--- frontend/add-student.js
+++ frontend/add-student.js
@@ -1,1 +1,55 @@
+// Add Student Function
 
+function addStudent(){
+
+
+    let student = {
+
+        id: Date.now(),
+
+        name: document.querySelector("#studentName").value,
+
+        father: document.querySelector("#fatherName").value,
+
+        mother: document.querySelector("#motherName").value,
+
+        className: document.querySelector("#className").value,
+
+        mobile: document.querySelector("#mobile").value,
+
+        address: document.querySelector("#address").value,
+
+        feeStatus: "Due",
+
+        attendance: "0%",
+
+        result: "Not Available"
+
+    };
+
+
+
+    let students = JSON.parse(
+        localStorage.getItem("students")
+    ) || [];
+
+
+
+    students.push(student);
+
+
+
+    localStorage.setItem(
+        "students",
+        JSON.stringify(students)
+    );
+
+
+
+    alert("Student Added Successfully");
+
+
+    window.location.href="students.html";
+
+
+}
