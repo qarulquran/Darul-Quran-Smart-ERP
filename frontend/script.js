9cdf7bac15261402f021d8e43908e57e61764fa2
qarulquran <darulquranam@gmail.com>
2026-08-19 14:33:42 +0600

    Update script.js
    Signed-off-by: qarulquran <darulquranam@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,66 +1,20 @@
-function login() {
+function login(){
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
 
 
-    if (username === "admin" && password === "12345") {
+    if(username === "admin" && password === "12345"){
 
         alert("Login Successful");
 
-        window.location.href = "dashboard.html";
+        window.location.href = "./dashboard.html";
 
-    } else {
+    }
+    else{
 
         alert("Invalid Username or Password");
 
     }
-
-}
-
-
-// Student Statistics Chart
-
-const studentChart = document.getElementById("studentChart");
-
-if (studentChart) {
-
-    new Chart(studentChart, {
-
-        type: "bar",
-
-        data: {
-
-            labels: [
-                "Class 1",
-                "Class 2",
-                "Class 3",
-                "Class 4",
-                "Class 5"
-            ],
-
-            datasets: [
-                {
-                    label: "Students",
-
-                    data: [
-                        25,
-                        35,
-                        40,
-                        30,
-                        45
-                    ]
-                }
-            ]
-
-        },
-
-        options: {
-
-            responsive: true
-
-        }
-
-    });
 
 }
