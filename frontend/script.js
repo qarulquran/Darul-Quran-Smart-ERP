cbaee86030fc1c6f45080b1074a3a6c6fdcd5bde
qarulquran <darulquranam@gmail.com>
2026-08-19 14:23:17 +0600

    Update script.js
    Signed-off-by: qarulquran <darulquranam@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,135 +1,66 @@
-function login(){
+function login() {
 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
 
-    if(username === "admin" && password === "12345"){
+    if (username === "admin" && password === "12345") {
 
         alert("Login Successful");
 
         window.location.href = "dashboard.html";
 
-    }
-
-    else{
+    } else {
 
         alert("Invalid Username or Password");
 
     }
 
-}// Student Statistics Chart
+}
+
+
+// Student Statistics Chart
 
 const studentChart = document.getElementById("studentChart");
 
-if(studentChart){
+if (studentChart) {
 
-new Chart(studentChart, {
+    new Chart(studentChart, {
 
-type: "bar",
+        type: "bar",
 
-data: {
+        data: {
 
-labels: [
-"Class 1",
-"Class 2",
-"Class 3",
-"Class 4",
-"Class 5"
-],
+            labels: [
+                "Class 1",
+                "Class 2",
+                "Class 3",
+                "Class 4",
+                "Class 5"
+            ],
 
-datasets:[{
+            datasets: [
+                {
+                    label: "Students",
 
-label:"Students",
+                    data: [
+                        25,
+                        35,
+                        40,
+                        30,
+                        45
+                    ]
+                }
+            ]
 
-data:[
-80,
-120,
-100,
-150,
-50
-]
+        },
 
-}]
+        options: {
 
-}
+            responsive: true
 
-});
+        }
 
-}
-
-
-
-// Attendance Chart
-
-const attendanceChart = document.getElementById("attendanceChart");
-
-if(attendanceChart){
-
-new Chart(attendanceChart, {
-
-type:"pie",
-
-data:{
-
-labels:[
-"Present",
-"Absent"
-],
-
-datasets:[{
-
-data:[
-90,
-10
-]
-
-}]
-
-}
-
-});
-
-}
-
-
-
-// Finance Chart
-
-const financeChart = document.getElementById("financeChart");
-
-if(financeChart){
-
-new Chart(financeChart, {
-
-type:"line",
-
-data:{
-
-labels:[
-"Jan",
-"Feb",
-"Mar",
-"Apr",
-"May"
-],
-
-datasets:[{
-
-label:"Income",
-
-data:[
-30000,
-45000,
-50000,
-60000,
-70000
-]
-
-}]
-
-}
-
-});
+    });
 
 }
