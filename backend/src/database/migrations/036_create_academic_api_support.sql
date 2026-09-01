8af2642575abf38bf341b33bd4a2eb65bf05821c
ISM <darulquranam@gmail.com>
2026-09-01 07:26:55 +0600

    Update 036_create_academic_api_support.sql
    Signed-off-by: ISM <darulquranam@gmail.com>

--- backend/src/database/migrations/036_create_academic_api_support.sql
+++ backend/src/database/migrations/036_create_academic_api_support.sql
@@ -12,9 +12,14 @@ CREATE INDEX IF NOT EXISTS idx_classes_institute_id
 ON classes (institute_id);
 
 CREATE INDEX IF NOT EXISTS idx_class_curriculum_institute_class
-ON class_curriculum (institute_id, class_id);
+ON class_curriculum (
+    institute_id,
+    class_id
+);
 
-CREATE INDEX IF NOT EXISTS idx_hifz_stages_institute_id
-ON hifz_stages (institute_id);
+CREATE INDEX IF NOT EXISTS idx_academic_department_stages_institute
+ON academic_department_stages (
+    institute_id
+);
 
 COMMIT;
